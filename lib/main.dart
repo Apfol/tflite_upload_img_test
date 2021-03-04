461472af797a9a02b09858eaaf089d3b5ac9d1ab
Andrés Ramos <andresfabi90@gmail.com>
2021-03-04 16:29:19 -0500

    Added send binary test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
 import 'package:image/image.dart' as imgLib;
@@ -71,17 +73,48 @@ class _MyHomePageState extends State<MyHomePage> {
   void _selectImage() async {
     final picker = ImagePicker();
     final pickedFile = await picker.getImage(source: ImageSource.gallery);
-    var image = imgLib.decodeImage(File(pickedFile.path).readAsBytesSync());
-    var thumbnail = imgLib.copyResizeCropSquare(image, 32);
-    File(pickedFile.path).writeAsBytesSync(imgLib.encodePng(thumbnail));
-    var recognitions = await Tflite.runModelOnImage(
-        path: pickedFile.path,   // required
-        imageMean: 0.0,   // defaults to 117.0
-        imageStd: 255.0,  // defaults to 1.0
+    var imageBytes = (File(pickedFile.path).readAsBytesSync().buffer.asByteData()).buffer;
+    imgLib.Image image = imgLib.decodeImage(imageBytes.asUint8List());
+    //var image = imgLib.decodeImage(File(pickedFile.path).readAsBytesSync());
+    var thumbnail = imgLib.copyResize(image, width: 32, height: 32);
+    //File(pickedFile.path).writeAsBytesSync(imgLib.encodePng(thumbnail));
+    var recognitions = await Tflite.runModelOnBinary(
+      binary: imageToByteListUint8(thumbnail, 300),
         numResults: 2,    // defaults to 5
         asynch: true      // defaults to true
     );
     print("TFLite Recognition :: ${recognitions.toString()}");
+  }
+
+  Uint8List imageToByteListFloat32(
+      imgLib.Image image, int inputSize, double mean, double std) {
+    var convertedBytes = Float32List(1 * inputSize * inputSize * 3);
+    var buffer = Float32List.view(convertedBytes.buffer);
+    int pixelIndex = 0;
+    for (var i = 0; i < inputSize; i++) {
+      for (var j = 0; j < inputSize; j++) {
+        var pixel = image.getPixel(j, i);
+        buffer[pixelIndex++] = (imgLib.getRed(pixel) - mean) / std;
+        buffer[pixelIndex++] = (imgLib.getGreen(pixel) - mean) / std;
+        buffer[pixelIndex++] = (imgLib.getBlue(pixel) - mean) / std;
+      }
+    }
+    return convertedBytes.buffer.asUint8List();
+  }
+
+  Uint8List imageToByteListUint8(imgLib.Image image, int inputSize) {
+    var convertedBytes = Uint8List(1 * inputSize * inputSize * 3);
+    var buffer = Uint8List.view(convertedBytes.buffer);
+    int pixelIndex = 0;
+    for (var i = 0; i < inputSize; i++) {
+      for (var j = 0; j < inputSize; j++) {
+        var pixel = image.getPixel(j, i);
+        buffer[pixelIndex++] = imgLib.getRed(pixel);
+        buffer[pixelIndex++] = imgLib.getGreen(pixel);
+        buffer[pixelIndex++] = imgLib.getBlue(pixel);
+      }
+    }
+    return convertedBytes.buffer.asUint8List();
   }
 
   @override
